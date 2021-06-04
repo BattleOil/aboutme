3fa444b89313bc4496d9bade5d3a7011a347485b
Ansel Huang <45125276+BattleOil@users.noreply.github.com>
2021-06-04 13:51:40 -0700

    Update Index_function.js

--- resource/js/Index_function.js
+++ resource/js/Index_function.js
@@ -1,8 +1,5 @@
-function ShowAboutMe() {
-  document.getElementById("aboutme").style.msTransform = "translateY(0%)"; // IE9 
-  document.getElementById("aboutme").style.transform = "translateY(0%)";
-}
-function CloseAboutMe() {
-  document.getElementById("aboutme").style.msTransform = "translateY(100%)"; // IE9 
-  document.getElementById("aboutme").style.transform = "translateY(100%)";
+function SetAboutMe(num) {
+  let val = num+'%';
+  document.getElementById("aboutme").style.msTransform = `translateY(${val})`; // IE9 
+  document.getElementById("aboutme").style.transform = `translateY(${val})`;
 }
