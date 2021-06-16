b9d9293a90ff717c7c4ee0865a12ecc65993ea49
Ansel <battleoil@outlook.com>
2021-06-16 14:41:13 -0700

    Improved page layout.

--- resource/js/Index_function.js
+++ resource/js/Index_function.js
@@ -9,4 +9,17 @@ function SetClosePosition() {
   if(w>2000){fr=(w-2000)/2;}else{fr=0;}
   document.getElementById('CloseAboutme').style.right = `${fr}px`;
 }
+function ChangeAnimation() {
+  document.getElementById("line1").style.msTransform = `rotate(45deg) translate(20px, -50px)`; // IE9 
+  document.getElementById("line1").style.transform = `rotate(45deg) translate(20px, -50px)`;
+  document.getElementById("line2").style.msTransform = `rotate(-45deg) translate(-50px, 20px)`; // IE9 
+  document.getElementById("line2").style.transform = `rotate(-45deg) translate(-50px, 20px)`;
+}
+
+function ResetAnimation() {
+  document.getElementById("line1").style.msTransform = `rotate(0deg) translate(0px, 0px)`; // IE9 
+  document.getElementById("line1").style.transform = `rotate(0deg) translate(0px, 0px)`;
+  document.getElementById("line2").style.msTransform = `rotate(0deg) translate(0px, 0px)`; // IE9 
+  document.getElementById("line2").style.transform = `rotate(0deg) translate(0px, 0px)`;
+}
 
